1b8d6dac6e5cc43fc68d88e355d32969fbca1d5e
Alexandrt Volkov <Alexandr1990@gitlab.com>
2021-08-31 13:03:58 +0300

    master: change FIELD_BASE_PAGE

--- backend/garpix_page/utils/fields_base_page.py
+++ backend/garpix_page/utils/fields_base_page.py
@@ -1,7 +1,4 @@
 FIELD_BASE_PAGE = [
-    'id', 'title', "slug",
-    "seo_title", "seo_title_ru", "seo_keywords",
-    "seo_keywords_ru", "seo_description", "seo_description_ru",
-    "seo_author", "seo_author_ru", "seo_og_type", "seo_image",
-    "parent"
+    'id', 'title', "slug", "seo_title", "seo_keywords", "seo_description",
+    "seo_author", "seo_og_type", "seo_image", "parent"
 ]
