5f0ae190bd1185dbcddcf95af5ef8cd5ae6ca17b
crusat <crusat@yandex.ru>
2022-07-25 10:09:05 +0300

    bump version

--- backend/garpix_page/setup.py
+++ backend/garpix_page/setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='garpix_page',
-    version='2.26.0',
+    version='2.27.0',
     description='',
     long_description=long_description,
     long_description_content_type='text/markdown',
