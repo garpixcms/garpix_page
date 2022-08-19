a092dc29b11760db5bbfe8bd86c796b2f2a06fae
Igor Sorokin <sorokin_i@garpix.com>
2022-08-19 14:42:20 +0300

    Improve styles, add ability to group components

--- backend/garpix_page/admin/forms.py
+++ backend/garpix_page/admin/forms.py
@@ -11,14 +11,40 @@ class AdminRadioSelectPreview(forms.RadioSelect):
         result = super().create_option(name, value, *args, **kwargs)
         ct = ContentType.objects.filter(pk=value).first()
         result['attrs']['preview'] = None
+        result['attrs']['group'] = 'Без категории'
         if ct is not None:
             model = apps.get_model(ct.app_label, ct.model)
             preview = getattr(model, 'admin_preview_image', None)
+            group = getattr(model, 'admin_group', 'Без категории')
             result['attrs']['preview'] = preview
+            result['attrs']['group'] = group
         return result
 
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context["widget"]["sorted_groups"] = self.sorted_groups(
+            context["widget"]["optgroups"]
+        )
+        context["test"] = self.choices
+        return context
+
+    def sorted_groups(self, optgroups):
+        groups = optgroups
+        result = {}
+        for group in groups:
+            groupKey = group[1][0]['attrs']['group']
+            if groupKey not in result:
+                result[groupKey] = []
+            result[groupKey].append(*group[1])
+
+        return result
 
 class PolymorphicModelPreviewChoiceForm(PolymorphicModelChoiceForm):
+    class Media:
+        css = {
+            'all': ('css/admin/styles.css',)
+        }
+
     type_label = 'Тип'
 
     ct_id = forms.ChoiceField(
