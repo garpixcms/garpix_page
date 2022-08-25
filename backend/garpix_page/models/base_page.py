159ce550de1e7e8d533fa79676277f9db28feb12
crusat <crusat@yandex.ru>
2022-08-25 12:37:15 +0300

    qa

--- backend/garpix_page/models/base_page.py
+++ backend/garpix_page/models/base_page.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.core.exceptions import ValidationError
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.functional import cached_property
