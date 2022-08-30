19d25f99f61325de3bf38380c40f02ab599d64bd
griviala <78097561+griviala@users.noreply.github.com>
2022-08-30 16:39:20 +0300

    2.32.0

--- backend/garpix_page/static/js/admin/clone.js
+++ backend/garpix_page/static/js/admin/clone.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
-    console.log(scrollbar);
     document.querySelector('[href="#openModal"]').addEventListener('click', function () {
         document.body.style.overflow = 'hidden';
         document.querySelector('#openModal').style.marginLeft = scrollbar;
