24d0df8429ad0cce2e1cca36fab359bd30f6bb36
griviala <78097561+griviala@users.noreply.github.com>
2022-10-03 15:46:30 +0300

    2.37.0

--- backend/garpix_page/utils/get_seo_value.py
+++ backend/garpix_page/utils/get_seo_value.py
@@ -3,10 +3,10 @@ def get_seo_value(page, field_name):
     from garpix_page.admin.settings.seo_template import SeoTemplateForm
     from garpix_page.models.settings import SeoTemplate
 
-    seo_templates = SeoTemplate.objects.all()
+    seo_templates = SeoTemplate.on_site.filter(is_active=True).all()
     for temp in seo_templates:
         if temp.rule_field == SeoTemplateForm.RULE_FIELD.MODEL_NAME and page.__class__.__name__ == temp.model_rule_value or str(
                 getattr(page, temp.rule_field, None)) == temp.rule_value:
-            return getattr(temp, field_name, '')
+            return getattr(temp, field_name, '').format(**page.__dict__)
 
     return ''
