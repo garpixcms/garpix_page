7056712807e60cf4d9e16bea8e6599e981fd2397
griviala <78097561+griviala@users.noreply.github.com>
2022-10-14 19:40:56 +0300

    2.38.0-rc7

--- backend/garpix_page/views/clear_cache.py
+++ backend/garpix_page/views/clear_cache.py
@@ -2,11 +2,12 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from garpix_page.cache import cache_service
 from django.urls import reverse
-from django.utils.translation import gettext as _
 
 
 def clear_cache(request):
+    if not request.user.is_authenticated or not request.user.is_staff:
+        return HttpResponseRedirect(reverse('admin:index'))
     cache_service.clear_all()
     link = request.META.get('HTTP_REFERER', reverse('admin:index'))
-    messages.add_message(request, messages.INFO, _('Кэш очищен'))
+    messages.add_message(request, messages.SUCCESS, 'Cache has been cleared', extra_tags='cache_cleared')
     return HttpResponseRedirect(link)
