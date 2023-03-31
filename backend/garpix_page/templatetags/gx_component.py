d67fefd30cb214ec571b95af465348256f9caa0f
Igor Sorokin <sorokin_i@garpix.com>
2023-03-31 15:05:07 +0300

    Check keys in `gx_component`

--- backend/garpix_page/templatetags/gx_component.py
+++ backend/garpix_page/templatetags/gx_component.py
@@ -5,7 +5,12 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def gx_component(context, component):
+    if not user.is_authenticated and not user.is_staff:
+        return ''
+
+    if not hasattr(context, 'user') or not hasattr(component, 'admin_edit_url'):
+        return ''
+
     user = context['user']
     component_path = component['admin_edit_url']
-
-    return f'data-gx-component={component_path}' if user.is_authenticated and user.is_staff else ''
+    return f'data-gx-component={component_path}'
