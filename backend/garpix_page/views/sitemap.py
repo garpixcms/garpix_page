42cb9bded31822a660fc17cb58741844762b5f2a
griviala <78097561+griviala@users.noreply.github.com>
2023-05-12 11:38:39 +0300

    2.46.0

--- backend/garpix_page/views/sitemap.py
+++ backend/garpix_page/views/sitemap.py
@@ -1,11 +1,12 @@
 from garpix_page.models import GarpixPageSiteConfiguration
-
-
 from django.contrib.sitemaps import GenericSitemap
 from garpix_page.models.base_page import BasePage
+from django.contrib.sitemaps.views import sitemap
 
 
-def sitemap_view():
+def sitemap_view(request, section=None,
+            template_name='sitemap.xml', content_type='application/xml'):
+
     pages = {
         'queryset': BasePage.on_site.filter(is_active=True, display_on_sitemap=True).order_by('title'),
         'date_field': 'created_at'
@@ -21,11 +22,10 @@ def sitemap_view():
     except Exception:
         pass
 
-    return {
-        'sitemaps': {
+    sitemaps = {
             'pages': GenericSitemap(
                 pages,
                 **extra_data
             ),
-        }
     }
+    return sitemap(request, sitemaps, section, template_name, content_type)
