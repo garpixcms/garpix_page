b42a8e8d7ee3ea6156d9fad3ab9efda1144bbb55
griviala <78097561+griviala@users.noreply.github.com>
2023-05-31 07:34:55 +0300

    2.48.0

--- backend/garpix_page/utils/set_children_urls.py
+++ backend/garpix_page/utils/set_children_urls.py
@@ -2,7 +2,8 @@
 def set_children_url(instance, children, pages_to_update):
     pages_to_update += list(children)
     for page in children:
-        page.set_url(parent=instance)
+        page.parent = instance
+        page.set_url()
         children = page.get_children()
         if any(children):
             set_children_url(page, children, pages_to_update)
